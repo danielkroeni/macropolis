a4581ee2e6a9b4f4f6704a30f2eb4e979c338f42
daniel.kroeni <daniel.kroeni@gmail.com>
2012-09-26 21:33:50 +0200

    using scala runtime mirror instead of java reflection check that field is actually on me

--- src/main/scala/macropolis/Macropolis.scala
+++ src/main/scala/macropolis/Macropolis.scala
@@ -11,46 +11,44 @@ object Macropolis {
 
   def mequals[T <: AnyRef](me: T, any: Any, firstField: Any, fields: Any*): Boolean = macro mequals_impl[T]
 
-  def mequals_impl[T <: AnyRef: c.AbsTypeTag](c: Context)(me: c.Expr[T], any: c.Expr[Any], firstField: c.Expr[Any], fields: c.Expr[Any]*): c.Expr[Boolean] = {
+  def mequals_impl[T <: AnyRef :c.AbsTypeTag](c: Context)(me: c.Expr[T], any: c.Expr[Any], firstField: c.Expr[Any], fields: c.Expr[Any]*): c.Expr[Boolean] = {
     import c.universe._
-
-
-    def meType = implicitly[c.AbsTypeTag[T]].tpe
+    // def meType = reify { implicitly[c.AbsTypeTag[T]].tpe }
 
     def compareFields: c.Expr[Boolean] = {
-      val otherAsT = c.universe.reify { val a = any.splice.asInstanceOf[T]; a}
+      val otherAsT = reify { any.asInstanceOf[T] }
 
       def compareSingleField(field: c.Expr[Any]): c.Expr[Boolean] = {
         val otherField = field.tree match {
-          case Select(This(_), fieldTerm) => c.Expr[Boolean](Select(otherAsT.tree, fieldTerm)) //  case Select(owner, fieldTerm) if owner == me.tree => ...
-          case i =>  c.abort(c.enclosingPosition, "Only simple field access is allowed! " + showRaw(i) + "      " + showRaw(me.tree))
+          case Select(owner:c.Tree, fieldTerm) if owner.symbol == me.tree.symbol => c.Expr[Boolean](Select(otherAsT.tree, fieldTerm))
+          case illegalField =>  c.abort(c.enclosingPosition, "Only simple field access is allowed! " + illegalField)
         }
         reify { field.splice == otherField.splice }
       }
 
-      fields.foldRight(compareSingleField(firstField)){ (f, expr) =>
-        reify { expr.splice && compareSingleField(f).splice } }
+      fields.foldRight(compareSingleField(firstField)) { (f, expr) =>
+        reify { expr.splice && compareSingleField(f).splice }
+      }
     }
 
     val e = reify {
-     if(any.splice.isInstanceOf[AnyRef]) {
-        val anyRef = any.splice.asInstanceOf[AnyRef]
-        if(me.splice.getClass.isAssignableFrom(anyRef.getClass)) {
-          compareFields.splice
-        } else {
-          false
-        }
-      } else {
-        false
+      val runtimeMirror = scala.reflect.runtime.currentMirror
+      val anyType = runtimeMirror.classSymbol(any.splice.getClass).typeSignature
+      val meType = runtimeMirror.classSymbol(me.splice.getClass).typeSignature
+
+      any.splice match {
+        case other: T if anyType <:< meType => (me.splice eq other) || compareFields.splice // here I would like to pass 'other: T' to compareFields
+        case _ => false
       }
     }
-    c.Expr[Boolean](c.resetAllAttrs(e.tree))
+
+    c.Expr[Boolean](c.resetAllAttrs(e.tree)) // what am I doing here?
   }
 
   def mhash(firstField: Any, fields: Any*): Int = macro mhash_impl
 
   def mhash_impl(c: Context)(firstField: c.Expr[Any], fields: c.Expr[Any]*): c.Expr[Int] = {
     import c.universe._
-    fields.foldRight(reify(41 + firstField.splice.hashCode)){ (f, expr) => reify((41 * expr.splice) + f.splice.hashCode) }
+    fields.foldRight(reify { 41 + firstField.splice.hashCode })( (f, expr) => reify { (41 * expr.splice) + f.splice.hashCode } )
   }
 }
